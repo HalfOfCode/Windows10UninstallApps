4fe79343fb3735dc5ea0239e088213a975d824ff
HalfOfCode <46304677+HalfOfCode@users.noreply.github.com>
2019-04-19 16:58:38 -0300

    Add files via upload

---
+++ desinstalarwindowsapps.ps1
@@ -0,0 +1,45 @@
+Get-AppxPackage -allusers *3dbuilder* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *windowsalarms* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *windowscamera* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *windowscommunicationsapps* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *officehub* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *getstarted* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *solit* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *skypeapp* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *zunemusic* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *windowsmaps* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *bingfinance* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *zunevideo* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *onenote* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *bingnews* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *people* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *windowsphone* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *photos* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *windowsstore* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *bingsports* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *soundrecorder* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *bingweather* | Remove-AppxPackage
+
+Get-AppxPackage -allusers *xboxapp* | Remove-AppxPackage
+
+
